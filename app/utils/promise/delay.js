ef440590def3709d47634206e15d9215f650e3b0
k03mad <k03.mad@gmail.com>
2021-01-02 21:19:55 +0300

    add debug delay

--- app/utils/promise/delay.js
+++ app/utils/promise/delay.js
@@ -1,7 +1,12 @@
 'use strict';
 
+const debug = require('debug')('utils-mad:promise:delay');
+
 /**
  * @param {number} time
  * @returns {Promise}
  */
-module.exports = (time = 1000) => new Promise(resolve => setTimeout(resolve, time));
+module.exports = (time = 1000) => {
+    debug(time);
+    return new Promise(resolve => setTimeout(resolve, time));
+};
