dd2f8c6fbc7c86b245c2223989425287bdb64ad7
k03mad <k03.mad@gmail.com>
2021-01-02 21:13:18 +0300

    update query next

--- app/api/next/query.js
+++ app/api/next/query.js
@@ -8,33 +8,26 @@ const {win} = require('../../const/ua');
 /**
  * @param {object} opts
  * @param {string} opts.path
- * @param {string} [opts.method]
  * @param {string} [opts.email]
  * @param {string} [opts.password]
  * @param {string} [opts.config]
- * @param {object} [opts.searchParams]
+ * @param {object} [opts.rest]
  * @returns {Array}
  */
 module.exports = async ({
     path,
-    method = 'GET',
+    rest,
     email = google.email,
     password = next.password,
     config = next.config,
-    searchParams = {
-        from: '-30d',
-        timezoneOffset: '-180',
-        selector: true,
-    },
 } = {}) => {
     const cookie = await auth({email, password});
     const {body} = await got(`https://api.nextdns.io/configurations/${config}/${path}`, {
-        method,
-        searchParams,
         headers: {
             'user-agent': win.chrome,
             cookie,
         },
+        ...rest,
     });
 
     return body;
