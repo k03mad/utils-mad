696479b6f1778f06e4070b1aae1de90674361806
k03mad <k03.mad@gmail.com>
2021-02-06 00:55:56 +0300

    escape up

--- app/utils/string/escape.js
+++ app/utils/string/escape.js
@@ -1,18 +1,11 @@
 'use strict';
 
-const convert = require('../array/convert');
-
 /**
  * @param {string} str
  * @param {Array} chars
  * @returns {string}
  */
-module.exports = (str, chars = [',', ' ']) => {
-    let escaped = str;
-
-    convert(chars).forEach(char => {
-        escaped = escaped.replace(new RegExp(char, 'g'), `\\${char}`);
-    });
-
-    return escaped;
-};
+module.exports = (str, chars = '*_`[') => str.replace(
+    new RegExp(`[${chars}]`, 'g'),
+    '\\$&',
+);
