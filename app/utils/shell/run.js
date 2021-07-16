b72e3bb4eb4c73a6b6f90e699fa1734dfec805e6
k03mad <k03.mad@gmail.com>
2021-07-17 01:53:08 +0300

    debug run

--- app/utils/shell/run.js
+++ app/utils/shell/run.js
@@ -2,6 +2,7 @@
 
 const convert = require('../array/convert');
 const cp = require('child_process');
+const debug = require('debug')('utils-mad:shell:run');
 const util = require('util');
 
 const exec = util.promisify(cp.exec);
@@ -12,10 +13,12 @@ const exec = util.promisify(cp.exec);
  */
 module.exports = async cmd => {
     const maxBuffer = 1024 * 5000;
-    const cmdArray = convert(cmd);
+
+    const run = convert(cmd).join(' && ');
+    debug('%o', run);
 
     try {
-        const {stdout, stderr} = await exec(cmdArray.join(' && '), {maxBuffer, shell: '/bin/bash'});
+        const {stdout, stderr} = await exec(run, {maxBuffer, shell: '/bin/bash'});
         return [stdout, stderr]
             .filter(Boolean)
             .map(elem => elem.trim())
@@ -23,7 +26,7 @@ module.exports = async cmd => {
     } catch (err) {
         throw new Error([
             'Error while trying to execute:',
-            `$ ${cmdArray.join('\n$ ')}`,
+            `$ ${run}`,
             `# code: ${err?.code}`,
             `# stdout: ${err?.stdout}`,
             `# stderr: ${err?.stderr}`,
